6ce6b8da17e821a666d574a07dc88ab95a609a4b
Reign <reignmaker.khubezhov@gmail.com>
2011-09-27 19:39:00 +0400

    Added backbone.js and underscore.js

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -8,8 +8,8 @@
 	<title>Институт цивилизации</title>
 	<?php endif ?>
 	<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
-<!--	<script type="text/javascript" src = "<?php echo base_url(); ?>assets/js/underscore-min.js"></script>
-	<script type="text/javascript" src = "<?php echo base_url(); ?>assets/js/backbone.js"></script> -->
+	<script type="text/javascript" src = "<?php echo base_url(); ?>assets/js/underscore-min.js"></script>
+	<script type="text/javascript" src = "<?php echo base_url(); ?>assets/js/backbone.js"></script>
 	<script type="text/javascript" src = "<?php echo base_url(); ?>assets/js/bootstrap-dropdown.js"></script>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
