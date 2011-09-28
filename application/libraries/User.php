d7462ff9d1b768bfab5e5657c8b74485704b11f5
Reign <reignmaker.khubezhov@gmail.com>
2011-09-28 20:50:36 +0400

    Reworked user class.

--- application/libraries/User.php
+++ application/libraries/User.php
@@ -4,15 +4,22 @@
 class User {
 
 	var $ci;
+    var $user;
 	public function __construct()
     {
     	$this->ci =& get_instance();
-    	$this->ci->load->library('session');
-    	    	
+    	//$this->ci->load->library('session');
+        if (!$this->ci->session->userdata('name')) {
+            $this->ci->session->set_userdata(array(
+                    'name' => 'Гость',
+                    'permission' => 'read',
+            ));
+        }
+    $this->user = $this->ci->session->all_userdata();
     }
     public function check_permission(){
     	    if ($this->ci->session->userdata('permission') != 'manage') {
-			redirect('/home/');
+			redirect(base_url());
 			echo "Not enough privileges.";
     	}
     }
