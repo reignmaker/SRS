f6ab34fdef1cc62bfce53b7b241a5a856fd674a5
Reign <reignmaker.khubezhov@gmail.com>
2011-09-29 21:15:03 +0400

    Basic student page functionality.

--- application/controllers/students.php
+++ application/controllers/students.php
@@ -7,6 +7,7 @@ class Students extends CI_Controller{
     {
     	parent::__construct();
 		$this->load->model('students_model');
+		$this->user->check_permission();
 		
 
     }
@@ -44,7 +45,6 @@ class Students extends CI_Controller{
 		$data['title'] = 'Список студентов';
 		$data['partial'] = 'partials/students';
 		$this->load->view('template',$data);
-		var_dump($data['panel']);
 	}
 	
 	
@@ -67,10 +67,43 @@ class Students extends CI_Controller{
 			'PR' => 'Связи с общественностью',
 			),);
 		$data['title'] = 'Добавить студента';
+		$data['panel'] = array('user' => $this->user->user,);
 		$data['partial'] = 'partials/add_students';
 		$this->load->view('template',$data);
 	}
-	public function add(){
+	function view($stid){
+		$data['student'] = $this->students_model->get_student($stid);
+		$data['student_subjects'] = $this->students_model->view($stid);
+		$data['options'] = array(
+				'course' => array(
+					'options' => array(
+						'0'  => 'Любой курс',
+						'1' => '1',
+						'2' => '2',
+						'3' => '3',
+						'4' => '4',
+						'5' => '5',
+						),
+				'selected' => $this->input->post('course'),
+				'default' => $data['student']->course,
+				),
+				'semester' => array(
+					'options' => array(
+						'1' => '1',
+						'2' => '2',
+						),
+					'selected' => $this->input->post('semester'),
+					'default' => '1',
+					),
+			);
+		
+		$data['partial'] = 'partials/student';
+		$data['panel'] = array('user' => $this->user->user,);
+/*		var_dump($data['options']['course']['default']);
+		var_dump($data['student']);*/
+		$this->load->view('template',$data);
+	}
+	function add(){
 		$data = array(
 			'fname' => $this->input->post('fname'),
 			'spec' => $this->input->post('spec'),
@@ -81,7 +114,7 @@ class Students extends CI_Controller{
 		echo "Студент ". $data['fname']. "добавлен в базу";
 		
 	}
-	public function ext(){
+	function ext(){
 		$condition = array(
 			'spec' => $this->input->post('spec')!='ANY' ? $this->input->post('spec'):'',
 			'course' => $this->input->post('course')!='0' ? $this->input->post('course'): '',
@@ -112,9 +145,5 @@ class Students extends CI_Controller{
 	}
 
 
-	function view($stid){
-		$data['student_subjects'] = $this->students_model->view($stid);
-		var_dump($data['student_subjects']->result());
-	}
 }
 
