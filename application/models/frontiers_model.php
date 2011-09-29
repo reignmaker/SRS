9c63dcb6254510526de8f49e80567779d0265d34
Reign <reignmaker.khubezhov@gmail.com>
2011-09-29 21:15:37 +0400

    Reworked to work with multiple options.

--- application/models/frontiers_model.php
+++ application/models/frontiers_model.php
@@ -2,7 +2,7 @@
 
 class Frontiers_model extends CI_Model{
 
-	public function get_frontiers($condition = null){
+	function get_frontiers($condition = null){
 		$cond =array();
 		foreach($condition as $key => $value){
 		
@@ -12,13 +12,15 @@ class Frontiers_model extends CI_Model{
 			}		
 		}
 		$this->db->select('students.id, students.fname');
-		if($condition['period'] == 1 or $condition['period'] == 'ANY')$this->db->select('f.cs_first');
-		if($condition['period'] == 2 or $condition['period'] == 'ANY')$this->db->select('f.fr_first');
-		if($condition['period'] == 3 or $condition['period'] == 'ANY')$this->db->select('f.cs_second');
-		if($condition['period'] == 4 or $condition['period'] == 'ANY')$this->db->select('f.fr_second');
-		if($condition['period'] == 5 or $condition['period'] == 'ANY')$this->db->select('f.cs_third');
-		if($condition['period'] == 6 or $condition['period'] == 'ANY')$this->db->select('f.cs_first,f.cs_second,f.cs_third');
-		if($condition['period'] == 7 or $condition['period'] == 'ANY')$this->db->select('f.fr_first, f.fr_second');
+
+		foreach ($condition['period'] as $period) {
+			if ($period == 'ANY') {
+				$this->db->select('f.cs_first, f.fr_first, f.cs_second, f.fr_second, f.cs_third');
+				break;
+			}else{
+				$this->db->select('f.'.$period);
+			}
+		}
 		$this->db->select('f.sbid');
 		$this->db->from('students');
 		$this->db->join('frontiers f','f.stid = students.id','left');
@@ -27,7 +29,7 @@ class Frontiers_model extends CI_Model{
 		return $q;
 	}
 
-	public function get_subjects_options($sbid = null){
+	function get_subjects_options($sbid = null){
 		$this->db->select('id, name')
 					->from('subjects');
 					if($sbid) $this->db->where(array('id' => $sbid));
@@ -35,7 +37,7 @@ class Frontiers_model extends CI_Model{
 		return $q;
 	}
 
-	public function set_mark($stid,$sbid,$data){
+	function set_mark($stid,$sbid,$data){
 		$this->db->where('stid',$stid);
 		$this->db->where('sbid',$sbid);
 		if($this->db->update('frontiers',$data)) return true;
